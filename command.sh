2d056bd280ef42d2c69495b12b27d25f95f608d0
Demir <130374789+Siradankullanici@users.noreply.github.com>
2023-04-25 21:32:43 +0300

    Add files via upload

--- command.sh
+++ command.sh
@@ -1,4 +1,5 @@
 sudo su
+sudo apt-get install dpkg
 sudo apt-get install hdparm && y
 sudo mkdir /mnt/mydrive
 sudo mount -o loop,rw  /media/kali/Ventoy/kali-linux-2023.1-installer-amd64.iso /mnt/mydrive
